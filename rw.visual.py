2be527ee44eae1643c835bcc1b26ee0c204b2a46
Tomas Makaj <Tmakajcoding@gmail.com>
2022-11-15 11:59:17 -0500

    Plotting Multiple Points

--- rw.visual.py
+++ rw.visual.py
@@ -37,3 +37,130 @@ while True:
         break
 # This code generates a random walk and displays it. When you close the viewer you'll be asked whether you want to generate another walk
 # Press y to generate walks that start near the starting point
+
+
+# Styling the Walk
+
+# In this section we will customize our plots to emphasize the important characteristics of each walk and deemphasize distracting elements.
+# We can identify where the walk began, where it ended, and the path taken
+
+# Coloring the points 
+
+# We'll use a colormap to show the order of the points in the walk and then remove the black outline from each dot so the color of the dots will be more clearer
+
+# Generate Multiple Random Walks
+
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active
+while True:
+    # Make a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    
+    
+    # Emphasize the first and last points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
+    
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+
+
+
+# # Cleaning up the Axes
+
+# # Lets remove the axes in this plot so they dont distract from the path of each walk. To turn of the axes use this code
+
+# # Remove the axes
+# # ax.get_xaxis().set_visible(False)
+# # ax.get_yaxis().set_visible(False)
+
+
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active
+while True:
+    # Make a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    
+    
+    # Emphasize the first and last points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)   
+    
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+    
+    
+    # Adding Plot Points
+
+# Lets increase the nnumber of points to give us more data to work with.
+# To do so we increase the value of num_points when we make a RandomWalk instance and adjust the size of each dot when drawing the plat, as shown.
+
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active
+while True:
+    # Make a random walk
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    
+    
+    # Emphasize the first and last points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)   
+    
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
+    
+
+# Altering the Size to Fill the Screen
+
+# A visualization is much more effective at communication patters in data if it fits nicely on the screen
+# To make the plotting window better fit your screen adjust the size of Matplotlibs output
+
+fig, ax = plt.subplots(figsize=(15, 9))
